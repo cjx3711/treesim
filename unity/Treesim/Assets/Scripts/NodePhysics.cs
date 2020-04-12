c59309579cc9265f4e361ce973258058c5a181ed
Jia Xun <chaijiaxun@gmail.com>
2020-04-12 16:04:29 -0700

    Skeleton of game, physics simulation.

---
+++ unity/Treesim/Assets/Scripts/NodePhysics.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodePhysics : MonoBehaviour {
+    public Vector3 velocity;
+    public Vector3 force;
+    // Start is called before the first frame update
+    void Start() {
+        velocity = new Vector3(1, 1, 0);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        // Friction
+        force -= velocity * 0.5f;
+        // Force affects velocity (a = f/m)
+        velocity += force * Time.deltaTime; // / node.size;
+        // Move node
+        transform.position = transform.position + (velocity * Time.deltaTime);
+        // Zero out forces for next frame
+        force = Vector3.zero;
+    }
+}
---
+++ unity/Treesim/Assets/Scripts/NodeSimulation.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeSimulation : MonoBehaviour {
+    public NodeSimulation root;
+    public List<NodeSimulation> links;
+    // Start is called before the first frame update
+    void Start() {
+        links = new List<NodeSimulation>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
